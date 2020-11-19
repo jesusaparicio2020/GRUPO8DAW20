c4ed5abdcb7fad0334024d8e5fa391a687ab8a50
Alexg256 <74537999+Alexg256@users.noreply.github.com>
2020-11-19 20:38:36 +0100

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -4,8 +4,8 @@
  {
      static public $mvc_bd_hostname = "localhost";
      static public $mvc_bd_nombre   = "alimentos2020";
-     static public $mvc_bd_usuario  = "myjesus95";
-     static public $mvc_bd_clave    = "Q1GuQDIX";
+     static public $mvc_bd_usuario  = "mypablocan";
+     static public $mvc_bd_clave    = "4iehlAHL";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
